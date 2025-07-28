7fe0158bca2333c2f9711673a4b5e0b55d9e6ebb
jldz9 <89154492+jldz9@users.noreply.github.com>
2025-07-28 17:10:14 -0600

    init commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# InSARScript
-Hassle-free InSAR processing scripts for multiple satellites
---
+++ src/insarscript/__init__.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+from colorama import Fore, Style, Back
+
+# For Sentinal-1 InSAR processing, ISCE2 and MintPy are used by default.
+# ---------------------Check GDAL and SQLite3 version---------------------
+# GDAL and SQLite3 are required by ISCE2 and MintPy
+# Use gdal > 3.8 and sqlite > 3.44 to avoid compatibility issues. (e.g.undefined symbol: sqlite3_total_changes64)
+from packaging.version import parse as v
+from osgeo import gdal
+gdal_version = gdal.__version__
+if v(gdal_version) < v('3.8'):
+    print(f"{Fore.RED}GDAL version {gdal_version} is not supported. Please install GDAL version >= 3.8.")
+else:
+    print(f"{Fore.GREEN}GDAL version {gdal_version} found.")
+
+import sqlite3
+sqlite_version = sqlite3.sqlite_version
+if v(sqlite_version) < v('3.44'):
+    print(f"{Fore.RED}SQLite version {sqlite_version} is not supported. Please install SQLite version >= 3.44.")
+else:
+    print(f"{Fore.GREEN}SQLite version {sqlite_version} found.")
+
+
+# ---------------------ISCE2 Configuration---------------------
+# If ISCE and Mintpy are proper installed, ISCE_HOME should exist in the environment
+# if ISCE_HOME exist, appending ISCE_HOME/applications and ISCE_HOME/bin to sys.path as recommended by MintPy 
+# https://github.com/yunjunz/conda-envs/blob/main/README.md#2-install-isce-2-and-mintpy
+try: 
+    import isce
+    print(f"{Fore.GREEN}ISCE2 version {isce.__version__} found.")
+except ImportError:
+    print(f"{Fore.RED}ISCE2 is not installed.")
+    sys.exit(1)
+
+if 'ISCE_HOME' not in os.environ:
+    print(f"{Fore.RED}Can not find environment variable ISCE_HOME. isce2 is either not installed or installed in customized path.")
+    sys.exit(1)
+else: 
+    isce_home = os.environ['ISCE_HOME']
+    application_path = isce_home + '/applications'
+    bin_path = isce_home + '/bin'
+    sys.path.append(application_path)
+    sys.path.append(bin_path)
+
+# ---------------------MintPy Configuration---------------------------
+# Configuration followed the MintPy post-installation setip 
+# https://github.com/insarlab/MintPy/blob/main/docs/installation.md#3-post-installation-setup
+
+try: 
+    import mintpy
+    print(f"{Fore.GREEN}MintPy version {mintpy.__version__} found.")
+except ImportError:
+    print(f"{Fore.RED}MintPy is not installed.")
+    sys.exit(1)
+# a. ERA5 for tropospheric correction
+#TODO - Add instruction for ERA5 setup at https://github.com/insarlab/pyaps#2-account-setup-for-era5
+
+# b. Dask for parallel processing
+from dask import config as dask_config
+tmp_dir = Path.home() / '.dask' / 'tmp'
+tmp_dir.mkdir(parents=True, exist_ok=True)
+dask_config.set({'temporary_directory':str(tmp_dir)})
+
+# c. Extra environment variables setup
+os.environ["VRT_SHARED_SOURCE"] = "0"
+os.environ["HDF5_DISABLE_VERSION_CHECK"] = "2"
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+

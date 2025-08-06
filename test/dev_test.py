41ede720be0863a6fbce1d577ff7585fe9331949
jldz9 <89154492+jldz9@users.noreply.github.com>
2025-08-06 16:35:55 -0600

    Add sentineleof for EOF download, Add S1_SLC child class for specific Sentinel 1 SLC download

--- test/dev_test.py
+++ test/dev_test.py
@@ -1,14 +1,23 @@
+
 import sys
 from pathlib import Path
 sys.path.append(Path(__file__).parent.parent.joinpath('src').as_posix())
 
 import asf_search as asf
 
-from insarscript.utils.tool import ASFDownloader
+from insarscript.utils.tool import S1_SLC
 
-a = ASFDownloader(
-    bbox=[-109.0, 36.0, -102.0, 41.0],
-    start_time="2022-01-01",
-    end_time="2022-01-31",
-    output_dir="downloads"
+a = S1_SLC(
+    platform=['Sentinel-1A', 'Sentinel-1B'],
+    maxResults=20,
+    AscendingflightDirection=True,
+    bbox = [-113.18, 37.77, -112.44, 38.10],
+    start='2017-01-01',
+    end='2019-12-30',
+    download_orbit=True,
+    output_dir = '~/S1'
 )
+a.search()
+a.download()
+
+
